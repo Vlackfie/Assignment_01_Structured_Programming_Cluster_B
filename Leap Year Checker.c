cb2a17495b0d90558cf038d5aec6fb6f73c61fb5
Sadikul Hossain <sadikulhossain45new@gmail.com>
2025-02-22 12:55:24 +0600

    Add files via upload

---
+++ Leap Year Checker.c
@@ -0,0 +1,15 @@
+/*Write a program that takes a year as input and checks whether
+it is a leap year or not using if-else conditions.*/
+
+#include <stdio.h>
+int main(){
+int Y;
+    scanf("%d",&Y);
+    if (((Y%4==0) && (Y%100!=0)) || (Y%400==0)){
+        printf("%d is a Leap Year", Y);
+    }
+    else
+        printf("%d is not a Leap Year", Y);
+
+return 0;
+}
---
+++ Odd or Even.c
@@ -0,0 +1,13 @@
+/*Write a C program to check whether a given number is odd or even
+using an if-else statement.*/
+#include <stdio.h>
+int main(){
+int a;
+    scanf("%d", &a);
+    if(a % 2 == 0){
+        printf("Even");
+}
+    else
+        printf("odd");
+return 0;
+}
