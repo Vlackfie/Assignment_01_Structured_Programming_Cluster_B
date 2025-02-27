a529879173d3748166430d31fb6d3565752d855e
Sadikul Hossain <sadikulhossain45new@gmail.com>
2025-02-27 11:38:24 +0600

    Add files via upload

---
+++ Vowel Consonant.c
@@ -0,0 +1,19 @@
+/*Problem: Vowel or Consonant
+Write a program to check if a character is a vowel or a consonant using the
+switch-case statement.*/
+
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    switch (ch){
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+        case 'A': case 'E': case 'I': case 'O': case 'U':
+            printf("%c is a vowel\n",ch);
+        break;
+        default:
+            printf("%c is a consonant\n",ch);
+    }
+return 0;
+}
+
