a3c12bd1fff75f140b9ec1df7a6f933d7b4170c3
Sadikul Hossain <sadikulhossain45new@gmail.com>
2025-02-27 15:57:28 +0600

    Add files via upload

---
+++ Sum of Array Elements.c
@@ -0,0 +1,19 @@
+ /*Problem: Sum of Array Elements
+ Write a program that takes an array of integers and finds the sum of its elements
+using a loop.*/
+
+#include <stdio.h>
+int main() {
+    int size, i;
+        scanf("%d", &size);
+        int arr[size];
+        for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+        int sum=0;
+        for (int i=0; i<size; i++){
+        sum+=arr[i];
+   }
+   printf("Sum of the arrar = %d\n", sum);
+    return 0;
+}
